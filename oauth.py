0f39098cc4589f24f843e1fd683296382f9f5da0
Amanda Folson <afolson@users.noreply.github.com>
2014-05-19 22:50:29 -0400

    Create oauth.py

---
+++ oauth.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# oauth.py -  Script to generate OAuth goodness with Tweepy
+# Copyright (C) 2013  Amanda Folson <amanda@incredibl.org>
+
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+import sys
+import tweepy
+
+CONSUMER_KEY = ''
+CONSUMER_SECRET = ''
+ACCESS_KEY = ''
+ACCESS_SECRET = ''
+
+auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
+api = tweepy.API(auth)
+api.update_status(sys.argv[1])
